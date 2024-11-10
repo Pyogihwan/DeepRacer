5722fad470f3bf380fc2e53d2a60272289699051
Pyogihwan <pghpgh1009@gmail.com>
2024-11-10 19:03:06 +0900

    UPDATE - rm calc save image

--- project/Calc/src/calc/aa/calc.cpp
+++ project/Calc/src/calc/aa/calc.cpp
@@ -105,26 +105,26 @@ void Calc::OnReceiveREvent(const deepracer::service::rawdata::proxy::events::REv
     std::vector<uint8_t> bufferR(bufferCombined.begin(), bufferCombined.begin() + 19200);
     std::vector<uint8_t> bufferL(bufferCombined.begin() + 19200, bufferCombined.end());
 
-    // m_logger.LogInfo() << "Calc::OnReceiveREvent - bufferR.size() = " << bufferR.size();
-    // m_logger.LogInfo() << "Calc::OnReceiveREvent - bufferL.size() = " << bufferL.size();
+    m_logger.LogInfo() << "Calc::OnReceiveREvent - bufferR.size() = " << bufferR.size();
+    m_logger.LogInfo() << "Calc::OnReceiveREvent - bufferL.size() = " << bufferL.size();
 
-    std::string data_path = "/home/ubuntu/test_socket_AA_data";
-
-    try {
-        if ((bufferR.size() != 160 * 120) || (bufferL.size() != 160 * 120)) {
-            m_logger.LogVerbose() << "Calc::OnReceiveREvent - Warning: received image size does not match expected size";
-            m_running = false;
-        }else{
-            cv::Mat imgR(120, 160, CV_8UC1, const_cast<uint8_t*>(bufferR.data()));
-            cv::Mat imgL(120, 160, CV_8UC1, const_cast<uint8_t*>(bufferL.data()));
-            cv::imwrite(data_path + "/" + "right" + "_" + std::to_string(bufferCombined[10000]) + ".png", imgR);
-            m_logger.LogInfo() << "Calc::OnReceiveREvent - Camera data ( right ) saved";
-            cv::imwrite(data_path + "/" + "left" + "_" + std::to_string(bufferCombined[10000]) + ".png", imgL);
-            m_logger.LogInfo() << "Calc::OnReceiveREvent - Camera data ( left ) saved";
-        }
-    } catch (const std::exception& e) {
-        m_logger.LogVerbose() << "Calc::OnReceiveREvent - Error saving camera data: " << e.what();
-    }
+    // // Image 저장
+    // std::string data_path = "/home/ubuntu/test_socket_AA_data";
+    // try {
+    //     if ((bufferR.size() != 160 * 120) || (bufferL.size() != 160 * 120)) {
+    //         m_logger.LogVerbose() << "Calc::OnReceiveREvent - Warning: received image size does not match expected size";
+    //         m_running = false;
+    //     }else{
+    //         cv::Mat imgR(120, 160, CV_8UC1, const_cast<uint8_t*>(bufferR.data()));
+    //         cv::Mat imgL(120, 160, CV_8UC1, const_cast<uint8_t*>(bufferL.data()));
+    //         cv::imwrite(data_path + "/" + "right" + "_" + std::to_string(bufferCombined[10000]) + ".png", imgR);
+    //         m_logger.LogInfo() << "Calc::OnReceiveREvent - Camera data ( right ) saved";
+    //         cv::imwrite(data_path + "/" + "left" + "_" + std::to_string(bufferCombined[10000]) + ".png", imgL);
+    //         m_logger.LogInfo() << "Calc::OnReceiveREvent - Camera data ( left ) saved";
+    //     }
+    // } catch (const std::exception& e) {
+    //     m_logger.LogVerbose() << "Calc::OnReceiveREvent - Error saving camera data: " << e.what();
+    // }
 
     // ControlData 서비스의 CEvent로 전송해야 할 값을 변경한다. 이 함수는 전송 타겟 값을 변경할 뿐 실제 전송은 다른 부분에서 진행된다.
     m_ControlData->WriteDataCEvent(sample);
